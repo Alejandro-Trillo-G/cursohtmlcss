f126acfe14eae6c2224cf6566818eaf5c40b071e
Alejandro-Trillo-G <alejandrotg2010@gmail.com>
2023-03-26 18:39:44 -0300

    ex026 de media query concluído

---
+++ exercicios/ex026/mq005/menu.js
@@ -0,0 +1,11 @@
+var botaoMenu = document.getElementById("burguer");
+botaoMenu.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "block";
+    }
+  }
